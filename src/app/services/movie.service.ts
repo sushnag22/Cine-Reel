bd7c692f9b36b934244555200fc4ef1c7c438de5
sushnag22 <sushruthnagaraj.github@gmail.com>
2023-07-27 19:44:24 +0530

    Update movie service with function to get movie details

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -3,6 +3,12 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
+export interface ApiResult {
+  page: number;
+  results: any[];
+  total_pages: number;
+  total_results: number;
+}
 @Injectable({
   providedIn: 'root'
 })
@@ -10,10 +16,14 @@ export class MovieService {
 
   constructor(private http: HttpClient) { }
 
-  getTopRatedMovies(): Observable<any> {
-    return this.http.get(`${environment.baseUrl}/movie/popular?api_key=${environment.apiKey}&language=en-US&page=1`);
+  getTopRatedMovies(page = 1): Observable<ApiResult> {
+    return this.http.get<ApiResult>(`${environment.baseUrl}/movie/popular?api_key=${environment.apiKey}&language=en-US&page=${page}`);
   }
 
-  getMovieDetails() {}
+  getMovieDetails(id: string) {
+    return this.http.get(
+      `${environment.baseUrl}/movie/${id}?api_key=${environment.apiKey}&language=en-US`
+    );
+  }
 
 }
