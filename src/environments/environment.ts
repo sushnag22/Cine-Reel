d50ffa8ef4e5a49659726f217975620ddbd52ec6
sushnag22 <sushruthnagaraj.github@gmail.com>
2023-07-27 12:55:29 +0530

    Add TMDB base URL, API key, and image URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'API_KEY_HERE',
+  baseUrl: 'https://api.themoviedb.org/3',
+  imageUrl: 'http://image.tmdb.org/t/p/'
 };
 
 /*
