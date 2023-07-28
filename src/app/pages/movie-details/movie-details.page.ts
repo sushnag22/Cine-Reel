3fec1018fc12984caadb83fadd61effc91811a6d
sushnag22 <sushruthnagaraj.github@gmail.com>
2023-07-28 20:53:46 +0530

    Update movie detail page logic

--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from 'src/app/services/movie.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-movie-details',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-details.page.scss'],
 })
 export class MovieDetailsPage implements OnInit {
+  movie = null;
+  imageBaseUrl = environment.imageUrl;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private movieService: MovieService) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.movieService.getMovieDetails(id).subscribe(res => {
+      console.log('details: ', res);
+      this.movie = res;
+    });
   }
+
+  openHomePage() {
+    window.open(this.movie.homepage);
+   }
 
 }
